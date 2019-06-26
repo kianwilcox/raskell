27800fc1a4b01892ea21d5aa0c79742ef2b3ddf1
Kian <kian@Caylyns-MacBook-Pro.local>
2019-06-25 18:49:44 -0700

    Foldls can now be added together to produce new Foldls that return the result of running every foldl only once, rather than running it once per foldl. They are otherwise identical to lambdas

--- raskell.rb
+++ raskell.rb
@@ -12,6 +12,30 @@ class Object
     self
   end
 
+  def apply(f)
+    f.(self)
+  end
+
+end
+
+class Integer
+  def foldr(func, unit)
+    i = 0
+    while i <= self
+      unit = func.(unit, i)
+      i+=1
+    end
+    unit
+  end
+
+  def foldl(func, unit)
+    i = self
+    while i >= 0
+      unit = func.(i, unit)
+      i-=1
+    end
+    unit
+  end
 end
 
 class Array
@@ -33,6 +57,14 @@ class Array
     else
       self.slice(n, self.length)
     end
+  end
+
+  def foldr(func, unit)
+    (self.length-1).foldr(->(idx, acc) { func(self[idx], acc) }, unit)
+  end
+
+  def foldl(func, unit)
+    (self.length-1).foldl(->(idx, acc) { func(self[idx], acc) }, unit)
   end
 end
 
@@ -82,4 +114,73 @@ class Proc
     lamb.(self.())
   end
 
+end
+
+class Foldl
+
+   alias_method :standard_ruby_kind_of?, :kind_of?
+
+   def kind_of?(clazz)
+     [Foldl, Proc].include?(clazz) || standard_ruby_kind_of(clazz)
+   end
+
+   def initialize(monoid) # must be [fn, unit] pair, a monoid
+     if monoid.first.kind_of?(Array)
+       @monoids = monoid
+     else
+       @monoids = [monoid]
+     end
+     self
+   end
+
+   def monoids
+     @monoids
+   end
+
+   def *(lamb)
+     ->(x) { self.( lamb.( x ) ) }
+   end
+
+   def |(lamb)
+     ->(x) { lamb.( self.( x ) ) }
+   end
+
+   def <=(val)
+    self.(val.())
+  end
+
+  def >=(lamb)
+    lamb.(self)
+  end
+
+   def +(foldl)
+     if foldl.kind_of?(Foldl)
+       Foldl.new(self.monoids + foldl.monoids)
+     else
+       raise "Cannot add two non-folds together"
+     end
+   end
+
+   def call(*vals)
+     if vals != nil && vals != [] && vals.first.class.method_defined?(:foldl)
+       val = vals.first
+       monoids = self.monoids
+       while val.length > 0
+         results = []
+         monoids = monoids.map do |monoid|
+           fn = monoid.first
+           unit = monoid.last
+           result = fn.(unit, val.first)
+           results << result
+           [fn, result]
+         end
+         val = val.drop(1)
+       end
+       results
+     elsif vals == []
+       self
+     else
+       raise "Cannot call Foldl on an object that does not have foldl defined."
+     end
+   end
 end
